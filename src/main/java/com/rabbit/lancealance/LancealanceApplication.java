e719db8e18a69c990b0b24884a19a76a93149de3
Laerte Guedes <laerteguedes8@gmail.com>
2019-12-28 11:14:59 -0300

    #1 estrutura inicial

---
+++ src/main/java/com/rabbit/lancealance/LancealanceApplication.java
@@ -0,0 +1,13 @@
+package com.rabbit.lancealance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancealanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancealanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabbit/lancealance/LancealanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbit.lancealance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LancealanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
